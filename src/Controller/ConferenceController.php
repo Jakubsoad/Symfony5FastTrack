7e20644383e9d5bcf71488329d1d71fdb999764c
Jakub Mróz <jakub.soad@gmail.com>
2020-11-02 21:53:00 +0100

    append lost files

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Entity\Conference;
+use App\Form\CommentFormType;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+use Twig\Error\LoaderError as LoaderErrorAlias;
+use Twig\Error\RuntimeError as RuntimeErrorAlias;
+use Twig\Error\SyntaxError as SyntaxErrorAlias;
+
+class ConferenceController extends AbstractController
+{
+    private Environment $twig;
+
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(Environment $twig, EntityManagerInterface $entityManager)
+    {
+        $this->twig = $twig;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @param ConferenceRepository $conferenceRepository
+     *
+     * @return Response
+     *
+     * @throws LoaderErrorAlias
+     * @throws RuntimeErrorAlias
+     * @throws SyntaxErrorAlias
+     *
+     * @Route("/", name="homepage")
+     */
+    public function index(ConferenceRepository $conferenceRepository)
+    {
+        return new Response($this->twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
+    }
+
+    /**
+     * @Route("/conference/{slug}", name="conference")
+     *
+     * @param Request $request
+     * @param Conference $conference
+     * @param CommentRepository $commentRepository
+     * @param ConferenceRepository $conferenceRepository
+     * @param string $photoDir
+     *
+     * @return Response
+     *
+     * @throws LoaderErrorAlias
+     * @throws RuntimeErrorAlias
+     * @throws SyntaxErrorAlias
+     */
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository, string $photoDir)
+    {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+
+            if ($photo = $form['photo']->getData()) {
+                try {
+                    $filename = bin2hex(random_bytes(6)) . '.' . $photo->guessExtension();
+                    $photo->move($photoDir, $filename);
+                    $comment->setPhotoFilename($filename);
+                } catch (FileException $e) {
+                    // unable to upload the photo, give up
+                } catch (Exception $e) {
+                }
+            }
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        return new Response($this->twig->render('conference/show.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form' => $form->createView(),
+            ]));
+    }
+}
