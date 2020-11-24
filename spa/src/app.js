87f168e6c981f2ecf263f174102317ab29334330
Jakub Mróz <jakub.soad@gmail.com>
2020-11-24 21:26:40 +0100

    spa app

---
+++ spa/src/app.js
@@ -0,0 +1,24 @@
+import {h, render} from 'preact';
+import Router from "preact-router";
+import Home from './pages/home';
+import Conference from "./pages/conference";
+
+function App() {
+    return (
+        <div>
+            <header>
+                <Link href="/">Home</Link>
+                <br />
+                <Link href="/conference/amsterdam2019">Amsterdam 2019</Link>
+            </header>
+
+            <Router>
+                <Home path="/" />
+                <Conference path="/conferece/:slug" />
+            </Router>
+        </div>
+    )
+}
+
+render(<App />, document.getElementById('app'));
+	   
